e6c7eca6307b4293f94fd92d2861903cc5fc943b
tomatoaas <tomomi.314159265@gmail.com>
2022-11-18 10:29:49 +0900

    [fix]calendar画像のhover時の動きをつけた。

--- app/src/components/DatePicker.jsx
+++ app/src/components/DatePicker.jsx
@@ -8,6 +8,7 @@ import "react-datepicker/dist/react-datepicker.css";
 registerLocale("ja", ja);
 
 const Com_DatePicker = () => {
+  // DatePicker
   const [startDate, setStartDate] = React.useState(new Date());
   const [isOpen, setIsOpen] = React.useState(false);
   const handleChange = (e) => {
@@ -19,8 +20,12 @@ const Com_DatePicker = () => {
     setIsOpen(!isOpen);
   };
 
+  // const imageUrl = "../../images/calendar.svg";
+  const [imageUrl, setImageUrl] = React.useState("../../images/calendar.svg");
+
+  // timePicker
   // start時間のhour
-  const [startHour, setStartHour] = React.useState();
+  const [startHour, setStartHour] = React.useState(8);
 
   const starthandleChange = (e) => {
     setStartHour(e.target.value);
@@ -31,6 +36,10 @@ const Com_DatePicker = () => {
 
   const endhandleChange = (e) => {
     setEndHour(e.target.value);
+  };
+
+  const SetEndHour = (run) => {
+    return run();
   };
 
   return (
@@ -44,13 +53,17 @@ const Com_DatePicker = () => {
             {format(startDate, "yyyy/MM/dd")}
             <img
               onClick={handleClick}
-              src="../../images/calendar.svg"
+              onMouseEnter={() =>
+                setImageUrl("../../images/calendar_hover.svg")
+              }
+              onMouseLeave={() => setImageUrl("../../images/calendar.svg")}
+              src={imageUrl}
               alt="calendar"
-              className={`object-contain w-5 flex ml-6`}
+              className={`object-contain w-5 flex ml-6 cursor-pointer`}
             />
           </div>
           {isOpen && (
-            <div className={`absolute`}>
+            <div className={`absolute z-10`}>
               <DatePicker
                 locale="ja"
                 selected={startDate}
@@ -62,14 +75,14 @@ const Com_DatePicker = () => {
           )}
 
           {/* 時間指定 */}
-          <div className={`flex flex-row justify-around mt-[15px]`}>
+          <div className={`flex flex-row justify-around mt-[10px]`}>
             <div className={`flex`}>開始時間</div>
             <div className={`flex`}>終了時間</div>
           </div>
           <div className={`flex justify-around mt-[10px] px-2`}>
             <div className={`flex`}>
               <select
-                className={`border-2 border-black`}
+                className={`border-2 border-black cursor-pointer rounded-md`}
                 name="start_hour"
                 id="start_hour"
                 onChange={(e) => starthandleChange(e)}
@@ -90,14 +103,12 @@ const Com_DatePicker = () => {
               </select>
               <div className={`px-1`}>:</div>
               <select
-                className={`border-2 border-black`}
+                className={`border-2 border-black cursor-pointer rounded-md`}
                 name="start_min"
                 id="start_min"
               >
                 <option value="00">00</option>
-                {startHour === "20" ? (
-                  ``
-                ) : (
+                {startHour !== "20" && (
                   <>
                     <option value="5">05</option>
                     <option value="10">10</option>
@@ -118,35 +129,35 @@ const Com_DatePicker = () => {
             <div>～</div>
             <div className={`flex`}>
               <select
-                className={`border-2 border-black`}
+                className={`border-2 border-black cursor-pointer rounded-md`}
                 name="end_hour"
                 id="end_hour"
                 onChange={(e) => endhandleChange(e)}
               >
-                <option value="08">08</option>
-                <option value="09">09</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
-                <option value="13">13</option>
-                <option value="14">14</option>
-                <option value="15">15</option>
-                <option value="16">16</option>
-                <option value="17">17</option>
-                <option value="18">18</option>
-                <option value="19">19</option>
-                <option value="20">20</option>
+                {SetEndHour(() => {
+                  if (startHour > endHour) {
+                    setEndHour(startHour);
+                  }
+                  const list = [];
+                  for (let i = startHour; i <= 20; i++) {
+                    console.log(i);
+                    list.push(
+                      <option value={i} key={i}>
+                        {("0" + i).slice(-2)}
+                      </option>
+                    );
+                  }
+                  return list;
+                })}
               </select>
               <div className={`px-1`}>:</div>
               <select
-                className={`border-2 border-black`}
+                className={`border-2 border-black cursor-pointer rounded-md`}
                 name="end_min"
                 id="end_min"
               >
                 <option value="00">00</option>
-                {endHour === "20" ? (
-                  ``
-                ) : (
+                {endHour !== "20" && (
                   <>
                     <option value="5">05</option>
                     <option value="10">10</option>
@@ -164,9 +175,44 @@ const Com_DatePicker = () => {
               </select>
             </div>
           </div>
-          <div className={`flex flex-row-reverse mt-[25px] mr-[30px] text-base`}>
+          <div className={`flex justify-between mt-[25px] mr-[30px] ml-[10px]`}>
             <div
-              className={`flex px-4 rounded-md drop-shadow-lg border-black bg-gray-300 border-2`}
+              className={`flex flex-row rounded-md bg-gray-200 border-[2px] border-black text-sm`}
+              x-data="app"
+            >
+              <div>
+                <input
+                  type="radio"
+                  name="option"
+                  id="1"
+                  className="peer hidden"
+                  defaultChecked
+                />
+                <label
+                  htmlFor="1"
+                  className={`my-[2px] ml-[2px] peer-checked:shadow-lg peer-checked:border-[1px] peer-checked:border-black block cursor-pointer select-none rounded-md w-[75px] text-center peer-checked:bg-white peer-checked:font-bold`}
+                >
+                  会議
+                </label>
+              </div>
+
+              <div>
+                <input
+                  type="radio"
+                  name="option"
+                  id="2"
+                  className={`peer hidden`}
+                />
+                <label
+                  htmlFor="2"
+                  className={`my-[2px] mr-[2px] block peer-checked:shadow-lg peer-checked:border-[1px] peer-checked:border-black cursor-pointer select-none rounded-md w-[80px] text-center peer-checked:bg-white peer-checked:font-bold`}
+                >
+                  サークル
+                </label>
+              </div>
+            </div>
+            <div
+              className={`flex px-4 pt-1 cursor-pointer font-bold text-sm rounded-md drop-shadow-lg border-black bg-[#c6ecff] hover:bg-[#a5e1ff] border-2`}
             >
               申請
             </div>
